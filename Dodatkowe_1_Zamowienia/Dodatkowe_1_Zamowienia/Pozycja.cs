081ab3703872cf735867dc011757eaa5aecd7653
Kuba <73018121+kakubus@users.noreply.github.com>
2022-10-24 20:52:41 +0200

    1

---
+++ Dodatkowe_1_Zamowienia/Dodatkowe_1_Zamowienia/Pozycja.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dodatkowe_1_Zamowienia
+{
+    class Pozycja
+    {
+        string _nazwaTowaru;
+        int _ileSztuk;
+        double _cena;
+
+        Pozycja(string nazwaTowaru, int ileSztuk, double cena)
+        {
+
+        }
+
+        public double obliczWartosc()
+        {
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+    }
+}
---
+++ Dodatkowe_1_Zamowienia/Dodatkowe_1_Zamowienia/Zamowienie.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dodatkowe_1_Zamowienia
+{
+    class Zamowienie
+    {
+        Pozycja[] _pozycje;
+        int _ileDodanych;
+        int _maksRozmiar;
+
+        Zamowienie()
+        {
+            _maksRozmiar = 10;
+        }
+
+        Zamowienie(int maksPozycji)
+        {
+            if(maksPozycji > 0)
+            {
+                _maksRozmiar = maksPozycji;
+            }
+            else
+            {
+                _maksRozmiar = 10;
+                Console.WriteLine("Error in max parameter. Set to default (10)");
+            }
+        }
+
+        public void dodajPozycje(Pozycja p)
+        {
+
+        }
+
+        public double obliczWartosc()
+        {
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
